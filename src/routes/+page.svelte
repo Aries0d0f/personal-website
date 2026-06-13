3abd2f32ff80c9e83fd4dfd3e69f232c604bdaa5
aries0d0f <aries0d0f@gmail.com>
2026-06-13 17:27:34 +0900

    feat(view): Setup home page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,218 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import gsap from 'gsap';
+	import { onMount } from 'svelte';
+	import { ParaglideMessage } from '@inlang/paraglide-js-svelte';
+
+	import Icon from '@iconify/svelte';
+
+	import { m } from '$lib/paraglide/messages.js';
+	import Avatar from '$lib/components/Avatar.svelte';
+
+	const contacts = [
+		{
+			name: 'Email',
+			url: 'mailto:aries0d0f@gmail.com',
+			icon: 'fa7-solid:envelope'
+		},
+		{
+			name: 'GitHub',
+			url: 'https://aries0d0f.me/s/github',
+			icon: 'fa7-brands:github'
+		},
+		{
+			name: 'Telegram',
+			url: 'https://aries0d0f.me/s/t.me',
+			icon: 'fa7-brands:telegram'
+		},
+		{
+			name: 'Medium',
+			url: 'https://medium.com/@aries0d0f',
+			icon: 'fa7-brands:medium'
+		},
+		{
+			name: 'LinkedIn',
+			url: 'https://aries0d0f.me/s/linkedin/in',
+			icon: 'fa7-brands:linkedin-in'
+		}
+	];
+</script>
+
+<div class="viewport-wrapper">
+	<main class="intro-container">
+		<div class="intro-avatar">
+			<Avatar />
+		</div>
+		<div class="intro-content">
+			<article>
+				<ParaglideMessage message={m.pages_home_profile_intro} inputs={{}}>
+					{#snippet strong({ children })}
+						<strong>
+							{@render children?.()}
+						</strong>
+					{/snippet}
+				</ParaglideMessage>
+			</article>
+			<footer>
+				<ul class="contact-container">
+					{#each contacts as contact (contact.name)}
+						<li>
+							<a
+								href={contact.url}
+								target="_blank"
+								rel="external noopener noreferrer"
+								class="contact"
+							>
+								<span>{contact.name}</span>
+								<Icon class="icon" icon={contact.icon} />
+							</a>
+						</li>
+					{/each}
+				</ul>
+				<p>
+					<span>&copy; {new Date().getFullYear()} {m.noun_general_name()}</span>
+					 &middot;
+					<a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="external noopener noreferrer">
+						<span>CC BY-SA 4.0</span>
+					</a>
+				</p>
+			</footer>
+		</div>
+	</main>
+</div>
+
+<style lang="scss">
+	.viewport-wrapper {
+		width: 100%;
+		min-height: 100vh;
+		display: flex;
+		place-items: center;
+		place-content: center;
+	}
+
+	.intro {
+		&-container {
+			display: flex;
+			place-items: center;
+			place-content: center;
+			flex-direction: row;
+			width: fit-content;
+			height: fit-content;
+
+			@media (max-width: 700px) {
+				flex-direction: column;
+				gap: 2rem;
+				place-content: start;
+
+				> .intro-content {
+					max-width: 80%;
+					padding: 1rem;
+
+					> footer {
+						margin-bottom: 0;
+						place-items: center;
+					}
+				}
+
+			}
+
+			@media (max-width: 414px) {
+				> .intro-content {
+					max-width: unset;
+					padding: 1rem;
+				}
+			}
+		}
+
+		&-avatar {
+			margin-top: -5rem;
+			transition: margin-top 0.3s ease;
+
+			@media ((max-height: 900px) and (orientation: portrait)) or (max-height: 500px) {
+				margin-top: 0;
+			}
+		}
+
+		&-content {
+			display: flex;
+			flex-direction: column;
+			max-width: 320px;
+			line-height: 1.6;
+			white-space: pre-wrap;
+			gap: 4rem;
+
+			> article {
+				font-size: 1rem;
+				color: #4d4d4d;
+			}
+
+			> footer {
+				display: flex;
+				width: 100%;
+				gap: 1rem;
+				margin-bottom: -4.5rem;
+				flex-direction: column;
+				place-items: end;
+				place-content: center;
+
+				p {
+					&,
+					> * {
+						color: #4d4d4d;
+						text-align: right;
+						font-size: 0.825rem;
+					}
+				}
+			}
+		}
+	}
+
+	.contact {
+		&-container {
+			display: flex;
+			gap: 1.6rem;
+			list-style: none;
+			place-content: center;
+			place-items: center;
+
+			@media (max-width: 300px) {
+				gap: 1rem;
+			}
+
+			> li {
+				display: inline-flex;
+			}
+		}
+
+		color: #000;
+		transition: color 0.3s ease;
+		position: relative;
+		text-decoration: none;
+		display: inline-flex;
+		place-content: center;
+
+		&:hover {
+			filter: drop-shadow(0 0 5px rgba(0, 0, 0, 0.25));
+		}
+
+		font-size: 28px;
+
+		> span {
+			position: absolute;
+			background: #000;
+			color: #fff;
+			padding: 0 0.4rem;
+			border-radius: 0.25rem;
+			opacity: 0;
+			transition: opacity 0.3s ease;
+			top: calc(100% + 0.4rem);
+			z-index: 1;
+			font-size: 0.8125rem;
+		}
+
+		&:hover {
+			> span {
+				opacity: 1;
+			}
+		}
+	}
+</style>
