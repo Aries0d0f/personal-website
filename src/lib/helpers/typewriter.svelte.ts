12bfb662e6c6ff549424b8a82fe3043bb862b865
aries0d0f <aries0d0f@gmail.com>
2026-07-11 20:14:24 +0900

    feat(helper): Support delete effect on typewritter.

--- src/lib/helpers/typewriter.svelte.ts
+++ src/lib/helpers/typewriter.svelte.ts
@@ -1,5 +1,6 @@
 const TYPING_DELAY_MAP: Record<string, number> = {
 	' ': 100,
+	'\b': 150,
 	'.': 200,
 	',': 200,
 	'!': 300,
@@ -10,21 +11,30 @@ const TYPING_DELAY_MAP: Record<string, number> = {
 const BASE_INTERVAL = 30;
 
 interface Options {
+	skipFirst?: boolean;
 	baseInterval?: number;
+	startAt?: number;
+	startDelay?: number;
 	delayMap?: Record<string, number>;
 }
 
 export const useTypewriter = (message: () => string, options: Options = {}) => {
-	const { baseInterval = BASE_INTERVAL, delayMap = TYPING_DELAY_MAP } = options;
+	const {
+		baseInterval = BASE_INTERVAL,
+		startAt = 0,
+		startDelay = 0,
+		delayMap = TYPING_DELAY_MAP,
+		skipFirst = false
+	} = options;
 
 	let text = $state('');
 	let isTyping = $state(false);
 	let intervalId = $state<ReturnType<typeof setInterval>>();
 
 	$effect(() => {
-		const chars = message().split('');
+		const chars = message().split('').slice(startAt);
 
-		text = '';
+		text = message().split('').slice(0, startAt).join('');
 		isTyping = true;
 		if (chars.length === 0) {
 			isTyping = false;
@@ -33,13 +43,19 @@ export const useTypewriter = (message: () => string, options: Options = {}) => {
 
 		let index = 0;
 		let elapsed = 0;
-		let nextTick = delayMap[chars[0]] ?? baseInterval;
+		let nextTick = (delayMap[chars[0]] ?? baseInterval) + startDelay;
 
 		intervalId = setInterval(() => {
 			elapsed += baseInterval;
 			if (elapsed < nextTick) return;
 
-			text += chars[index];
+			const char = chars[index];
+			if (char === '\b') {
+				text = text.slice(0, -1);
+			} else {
+				text += char;
+			}
+
 			index++;
 
 			if (index >= chars.length) {
@@ -48,7 +64,7 @@ export const useTypewriter = (message: () => string, options: Options = {}) => {
 				return;
 			}
 
-			nextTick += delayMap[chars[index]] ?? baseInterval;
+			nextTick += delayMap[char] ?? baseInterval;
 		}, baseInterval);
 
 		return () => {
@@ -62,6 +78,13 @@ export const useTypewriter = (message: () => string, options: Options = {}) => {
 		text = message();
 		isTyping = false;
 	};
+
+	if (skipFirst) {
+		// Skip the first typing effect
+		setTimeout(() => {
+			skip();
+		}, 1);
+	}
 
 	return {
 		skip,
