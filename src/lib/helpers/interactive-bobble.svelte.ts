dde3a71fa7c69f879d1db83e1ba26a72009e5085
aries0d0f <aries0d0f@gmail.com>
2026-08-28 20:16:25 +0900

    style: Remove debug console.

--- src/lib/helpers/interactive-bobble.svelte.ts
+++ src/lib/helpers/interactive-bobble.svelte.ts
@@ -17,21 +17,17 @@ export const useInteractiveBobble = (ignore: () => boolean) => {
             icon: hoveredElement.getAttribute('data-bobble-icon') || undefined,
             link: hoveredElement.getAttribute('data-bobble-link') || undefined
         }
-        console.log('Currently hovering:', hoveredElement, 'Bobble:', newBobbleContent);
+
         if (newBobbleContent.message || newBobbleContent.icon) {
-            console.log('Setting bobble content for hovered element:', hoveredElement, 'Bobble:', newBobbleContent);
             clearTimeout(bobbleClearTimeout);
             // Skip setting bobble if the content is the same as the current bobble
             if (bobble?.message === newBobbleContent.message && bobble?.icon === newBobbleContent.icon) {
-                console.log('Bobble content is the same as current, skipping update.');
                 return;
             }
             bobble = newBobbleContent;
         } else if (hoveredElement.getAttribute('aria-label') === 'Bobble Message') {
-            console.log('Hovered over the bobble itself, keeping current bobble content.');
             clearTimeout(bobbleClearTimeout);
         } else {
-            console.log('No bobble content found for hovered element:', hoveredElement);
             clearTimeout(bobbleClearTimeout);
             bobbleClearTimeout = setTimeout(() => {
                 bobble = undefined;
